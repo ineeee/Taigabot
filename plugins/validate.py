4fad3cd464e5357c9cb656fcb84301b202b78785
inexist3nce <inexist3nce@users.noreply.github.com>
2022-04-26 03:37:11 +0000

    validate.py: update w3c validator url, better error handling

--- plugins/validate.py
+++ plugins/validate.py
@@ -12,10 +12,19 @@ def validate(inp):
     if not inp.startswith('http'):
         inp = 'https://' + inp
 
-    url = 'https://validator.w3.org/nu/?doc=' + request.urlencode(inp)
+    url = 'https://validator.nu/?out=xhtml&doc=' + request.urlencode(inp)
     html = request.get(url)
     soup = BeautifulSoup(html, 'lxml')
+
+    sys_errors = soup.find('li', attrs={'class': 'non-document-error'})
+
+    if sys_errors is not None:
+        return "[w3c] The validator returned an error: {}".format(sys_errors.get_text())
+
     results = soup.find('div', attrs={'id': 'results'})
+
+    if results is None:
+        return "[w3c] The validator didn't return anything"
 
     errors = len(results.find_all('li', attrs={'class': 'error'}))
     warns = len(results.find_all('li', attrs={'class': 'warning'}))
