0b1e13eaeb331d6e17591361a46f08001c1e56ca
inexist3nce <inexist3nce@users.noreply.github.com>
2022-04-26 03:37:50 +0000

    debt.py: update url

--- plugins/debt.py
+++ plugins/debt.py
@@ -1,4 +1,5 @@
 # us debt plugin by ine (2020)
+# checked 04/2022
 from util import hook
 from utilities import request
 from bs4 import BeautifulSoup
@@ -18,8 +19,8 @@ def parse(html):
 def debt(inp):
     """debt -- returns the us national debt"""
 
-    url = "https://commodity.com/debt-clock/us/"
+    url = "https://commodity.com/data/usa/debt-clock/"
     html = request.get(url)
-    debt = parse(html)
+    amount = parse(html)
 
-    return "Current US Debt: \x02{}\x02".format(debt)
+    return "Current US Debt: \x02{}\x02".format(amount)
