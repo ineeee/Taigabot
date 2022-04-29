b524fb3c3923bf8d49abfb80a4c97142c587c988
inexist3nce <inexist3nce@users.noreply.github.com>
2022-04-29 03:50:39 +0000

    fmylife.py: update scraper, don't download data at startup, alias

--- plugins/fmylife.py
+++ plugins/fmylife.py
@@ -1,4 +1,5 @@
 # fuck my life plugin by ine (2020)
+# updated 04/2022
 from util import hook
 from utilities import request
 from bs4 import BeautifulSoup
@@ -10,7 +11,7 @@ def refresh_cache():
     print "[+] refreshing fmylife cache"
     html = request.get('https://www.fmylife.com/random')
     soup = BeautifulSoup(html, 'lxml')
-    posts = soup.find_all('a', attrs={'class': 'article-link'})
+    posts = soup.find_all('a', attrs={'class': 'block text-blue-500 my-4'})
 
     for post in posts:
         id = post['href'].split('_')[1].split('.')[0]
@@ -18,6 +19,7 @@ def refresh_cache():
         cache.append((id, text))
 
 
+@hook.command("fmylife", autohelp=False)
 @hook.command(autohelp=False)
 def fml(inp):
     "fml -- Gets a random quote from fmyfife.com."
@@ -27,6 +29,3 @@ def fml(inp):
 
     id, text = cache.pop()
     return '(#{}) {}'.format(id, text)
-
-
-refresh_cache()
