929edecbd7472204bd78ae701b4cc99bf84572ac
inexist3nce <inexist3nce@users.noreply.github.com>
2022-06-11 06:49:54 +0000

    util/web.py: remove offline yahoo, hastebin services, drop urlnorm

--- plugins/util/web.py
+++ plugins/util/web.py
@@ -1,8 +1,4 @@
 """ web.py - handy functions for web services """
-from __future__ import print_function
-from __future__ import absolute_import
-
-from . import urlnorm
 import requests
 
 
@@ -15,9 +11,8 @@ class ShortenError(Exception):
         return self.text
 
 
-def isgd(url):
+def isgd(url: str):
     """ shortens a URL with the is.gd API """
-    url = urlnorm.normalize(url, assume_scheme='http')
     req = requests.get("http://is.gd/create.php", params={'format': 'json', 'url': url})
 
     try:
@@ -34,24 +29,3 @@ def isgd(url):
 
 def try_isgd(url):
     return isgd(url)
-
-
-def haste(text, ext='txt'):
-    """ pastes text to a hastebin server """
-    req = requests.post("https://hastebin.com/documents", data=text)
-
-    if req.status_code >= 500 and req.status_code < 600:
-        print("[!] ERROR: hastebin is down")
-        return "(error: hastebin is down)"
-
-    try:
-        data = req.json()
-        return "https://hastebin.com/raw/{}.{}".format(data['key'], ext)
-    except ValueError:
-        print("[!] ERROR: hastebin returned invalid json")
-        return "(error: hastebin is broken)"
-
-
-def query(query, params={}):
-    print("[!] ERROR: yql is unavailable but being called")
-    return None
