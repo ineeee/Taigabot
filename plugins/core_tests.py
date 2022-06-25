dc573a7880e411d3d4e3cd29aa84885e07cddb05
inexist3nce <inexist3nce@users.noreply.github.com>
2022-06-25 18:46:41 +0000

    core_tests: check for format. read commit desc.
    i've decided to enforce the docstring syntax to be
        ^([a-z0-9]+)( <[0-9a-z #@|]+>| \[[0-9a-z #@|]+\])*( -- |: )(.+)$
    which translates to
        command <required> [optional] -- description

--- plugins/core_tests.py
+++ plugins/core_tests.py
@@ -1,3 +1,5 @@
+import re
+
 from util import hook
 
 
@@ -10,18 +12,27 @@ def test_docstrings(inp, bot):
     # - multiline docstring: the function has a multiline string and it can't be sent over irc
 
     failed = []
+    count = 0
 
     for command, (func, args) in bot.commands.items():
+        count += 1
+
         if func.__doc__ is None:
             failed.append(('missing', command, f'{func._filename}:{func.__code__.co_firstlineno}'))
         elif '\n' in func.__doc__:
             failed.append(('multiline', command, f'{func._filename}:{func.__code__.co_firstlineno}'))
+        else:
+            regex = r'^([a-z0-9]+)( <[0-9a-z #@|]+>| \[[0-9a-z #@|]+\])*( -- |: )(.+)$'
+            match = re.search(regex, func.__doc__)
+
+            if match is None:
+                failed.append(('format', command, f'{func._filename}:{func.__code__.co_firstlineno} {func.__doc__}'))
 
     if len(failed) == 0:
-        return 'all commands passed the test'
+        return f'all {count} commands passed the test'
 
     print('commands that failed the docstring test:')
     for (reason, command, source) in failed:
         print(f'- {reason}: {command} defined in {source}')
 
-    return f'there are {len(failed)} commands that did not pass the test'
+    return f'[TESTS] there are {len(failed)} commands that did not pass the test, out of {count} total'
