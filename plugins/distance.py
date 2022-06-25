093b68898cd44e1dea5f47a8675a566bc3a84c12
inexist3nce <inexist3nce@users.noreply.github.com>
2022-06-25 13:01:03 +0000

    distance: rm unicode prefix, use fstrings, use single quotes, add type hinting

--- plugins/distance.py
+++ plugins/distance.py
@@ -5,10 +5,10 @@ from utilities import request
 from bs4 import BeautifulSoup
 
 
-def fetch(start, dest):
+def fetch(start: str, dest: str):
     start = request.urlencode(start)
     dest = request.urlencode(dest)
-    url = "https://www.travelmath.com/flying-distance/from/{}/to/{}".format(start, dest)
+    url = f'https://www.travelmath.com/flying-distance/from/{start}/to/{dest}'
     html = request.get(url)
     return html
 
@@ -29,20 +29,20 @@ def parse(html):
 
 @hook.command
 def distance(inp):
-    "distance <start> to <end> -- Calculate the distance between 2 places."
+    """distance <start> to <end> -- Calculate the distance between 2 places."""
     if 'from ' in inp:
         inp = inp.replace('from ', '')
 
     if ' to ' not in inp:
         return '[Distance] You need to specify 2 places like so: "from place1 to place2"'
 
-    start = inp.split(" to ")[0].strip()
-    dest = inp.split(" to ")[1].strip()
+    start = inp.split(' to ')[0].strip()
+    dest = inp.split(' to ')[1].strip()
 
     html = fetch(start, dest)
     query, distance = parse(html)
 
     if not distance:
-        return u'[Distance] Could not calculate the distance from "{}" to "{}".'.format(start, dest)
+        return f'[Distance] Could not calculate the distance from "{start}" to "{dest}".'
 
-    return u'[Distance] {} {}'.format(query, distance)
+    return f'[Distance] {query} {distance}'
