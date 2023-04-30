964a2300053617e4d4b2cc97f0ebed3d7c5c23ae
ineeee <inexist3nce@gmail.com>
2023-04-30 05:08:11 +0000

    wordoftheday: fix scraper on new html

--- plugins/wordoftheday.py
+++ plugins/wordoftheday.py
@@ -1,5 +1,5 @@
 # word of the day plugin by ine (2020)
-# checked 04/2022
+# checked 04/2023
 from util import hook
 from utilities import request, iterable
 from bs4 import BeautifulSoup
@@ -10,7 +10,7 @@ def wordoftheday(inp):
     html = request.get('https://www.merriam-webster.com/word-of-the-day')
     soup = BeautifulSoup(html)
 
-    word = soup.find('div', attrs={'class': 'word-and-pronunciation'}).find('h1').text
+    word = soup.find('div', attrs={'class': 'word-and-pronunciation'}).find('h2').text
     paragraphs = soup.find('div', attrs={'class': 'wod-definition-container'}).find_all('p')
 
     definitions = []
@@ -21,7 +21,7 @@ def wordoftheday(inp):
     definitions = '; '.join(definitions)
     output = f'The word of the day is \x02{word}\x02: {definitions}'
 
-    if len(output) > 300:
-        output = output[:300] + '... More at https://www.merriam-webster.com/word-of-the-day'
+    if len(output) > 240:
+        output = output[:240] + '... More at https://www.merriam-webster.com/word-of-the-day'
 
     return output
